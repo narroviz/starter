b6fc778c9ce6a2ef6880b556888d266467093628
dmnewcomb <dnewcomb23@gmail.com>
2020-06-02 20:51:33 -0700

    Standardizing header, adding footer, adapting fonts for template

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,8 +1,8 @@
 const fallbackData = [
-      {"image":"nba","url":"2020/05/nba-recordigami","hed":"NBA Recordigami"},
-      {"image":"nba","url":"2020/05/nba-random-walk","hed":"NBA Random Walk"},
-      {"image":"wnba","url":"2020/05/wnba-recordigami","hed":"WNBA Recordigami"},
-      {"image":"wnba","url":"2020/05/wnba-random-walk","hed":"WNBA Random Walk"},
+      {}, 
+      {"image":"nba","url":"html/nba-season-history","hed":"NBA Season History"},
+      {"image":"nba","url":"html/wnba-season-history","hed":"WNBA Season History"},
+      {},
     ]
 
 let storyData = null;
@@ -21,41 +21,61 @@ function loadJS(src, cb) {
   return script;
 }
 
+
 function loadStories(cb) {
-  const request = new XMLHttpRequest();
-  const v = Date.now();
-  const url = `http://wanderwhim.com/assets/data/stories.json?v=${v}`;
+  var request = new XMLHttpRequest();
+  var v = Date.now();
+  var url = "https://wanderwhim.com/assets/data/stories.json?v=".concat(v);
   request.open("GET", url, true);
 
-  request.onload = () => {
+  request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
-      const data = JSON.parse(request.responseText);
+      var data = JSON.parse(request.responseText);
       cb(data);
     } else cb(fallbackData);
   };
 
-  request.onerror = () => cb(fallbackData);
+  request.onerror = function () {
+    return cb(fallbackData);
+  };
 
   request.send();
 }
 
 function createLink(d) {
-  return `
-	<a class='footer-recirc__article' href='http://wanderwhim.com/${d.url}' target='_blank' rel='noopener'>
-		<img class='article__img' src='./../assets/images/${d.image}.png' alt='${d.hed}'>
-		<p class='article__headline'>${d.hed}</p>
-	</a>
-	`;
+  if (d.url !== " ") {
+    return "\n\t<a class='footer-recirc__article' href='https://wanderwhim.com/".concat(d.url, "' target='_blank' rel='noopener'>\n\t\t<img class='article__img' src='./../assets/images/").concat(d.image, ".png' alt='").concat(d.hed, "'>\n\t\t<p class='article__headline'>").concat(d.hed, "</p>\n\t</a>\n\t");
+  } else {
+    return "\n\t<a class='footer-recirc__article' target='_blank' rel='noopener'>\n\t\t<img class='article__img'".concat("' alt='").concat(d.hed, "'>\n\t\t<p class='article__headline'>").concat(d.hed, "</p>\n\t</a>\n\t");
+  }
 }
 
 function recircHTML() {
-  const url = window.location.href;
-  const html = storyData
-    .filter(d => !url.includes(d.url))
-    .slice(0, 4)
-    .map(createLink)
-    .join("");
+  const storyData = [
+  {
+    "image": "",
+    "url": " ",
+    "hed": ""
+  },
+  {
+    "image": "wnba_season_paths",
+    "url": "html/wnba-season-history",
+    "hed": "WNBA Season History"
+  }, {
+    "image": "nba_season_paths",
+    "url": "html/nba-season-history",
+    "hed": "NBA Season History"
+  },
+  {
+    "image": "",
+    "url": " ",
+    "hed": ""
+  }];
 
+  var url = window.location.href;
+  var html = storyData.filter(function (d) {
+    return !url.includes(d.url);
+  }).slice(0, 4).map(createLink).join("");
   d3.select(".wanderwhim-footer .footer-recirc__articles").html(html);
 }
 
