0c62bda01e825502d94cccc4ad14aab35fe4cfbb
dmnewcomb <dnewcomb23@gmail.com>
2020-07-15 16:44:18 -0700

    Updating starter to have some more robust dealings

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,7 +1,12 @@
 /* global d3 */
+import isMobile from "./utils/is-mobile";
+
 function resize() {}
 
 function init() {
+	if(isMobile.any()) {
+		d3.select(".intro").style("padding-top", "4.5rem");
+	}
   console.log('Make something awesome!');
 }
 
